805b98712de9b6ebdb4b6ecc9d711d9ab8870e9d
HY-youn <yhd9417@gmail.com>
2024-10-15 20:40:56 +0900

    wordsquare word random extract

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(function(){
+
+
+const container = $('.container');
+const wsPlaybtn = $('.ws-play-btn');
+const wsBackbtn = $('.ws-back-btn-word');
+// const container = document.querySelector('.container');
+// const wsPlaybtn = document.querySelector('.ws-play-btn');
+
+// wsPlaybtn.addEventListener('click', function(){
+//   container.classList.add('scrolldown');
+// })
+wsPlaybtn.on('click', function(){
+  container.addClass('scrolldown');
+})
+wsBackbtn.on('click', function(){
+  container.removeClass('scrolldown');
+})
+});
