53053356cd5f94cbd65309614356d36cb5454220
HY-youn <yhd9417@gmail.com>
2024-10-22 06:16:53 +0900

    add 1021 chief of education sermon

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
-$(function(){
+// $(function(){
 
 
-const container = $('.container');
-const wsPlaybtn = $('.ws-play-btn');
-const wsBackbtn = $('.ws-back-btn-word');
-// const container = document.querySelector('.container');
-// const wsPlaybtn = document.querySelector('.ws-play-btn');
+// const container = $('.container');
+// const wsPlaybtn = $('.ws-play-btn');
+// const wsBackbtn = $('.ws-back-btn-word');
+// // const container = document.querySelector('.container');
+// // const wsPlaybtn = document.querySelector('.ws-play-btn');
 
-// wsPlaybtn.addEventListener('click', function(){
-//   container.classList.add('scrolldown');
+// // wsPlaybtn.addEventListener('click', function(){
+// //   container.classList.add('scrolldown');
+// // })
+// wsPlaybtn.on('click', function(){
+//   container.addClass('scrolldown');
 // })
-wsPlaybtn.on('click', function(){
-  container.addClass('scrolldown');
-})
-wsBackbtn.on('click', function(){
-  container.removeClass('scrolldown');
-})
-});
+// wsBackbtn.on('click', function(){
+//   container.removeClass('scrolldown');
+// })
+// });
